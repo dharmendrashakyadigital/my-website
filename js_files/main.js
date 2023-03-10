c4987c0395bb518f93ac63a3cb84a7dbdc40dcc8
Dharmendra Singh <126655790+dharmendrashakyadigital@users.noreply.github.com>
2023-03-10 14:20:43 +0530

    Initial commit

---
+++ js_files/main.js
@@ -0,0 +1,86 @@
+$(".figma-1").owlCarousel({
+  autoplay: true,
+  slideSpeed: 1000,
+  items: 3,
+  loop: true,
+  nav: true,
+  navText: [
+    '<i class="fa fa-arrow-left fa-2x"></i>',
+    '<i class="fa fa-arrow-right fa-2x"></i>'
+  ],
+  margin: 30,
+  dots: true,
+  responsive: {
+    320: {
+      items: 1
+    },
+    767: {
+      items: 2
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+// <!-- <<<<<<<<<<<<<<< meet doctors>>>>>>>>>>> -->
+
+$(".figma-20").owlCarousel({
+  autoplay: true,
+  slideSpeed: 1000,
+  items: 3,
+  loop: true,
+  nav: true,
+  navText: [
+    '<i class="fa fa-arrow-left fa-2x"></i>',
+    '<i class="fa fa-arrow-right fa-2x"></i>'
+  ],
+  margin: 30,
+  dots: true,
+  responsive: {
+    320: {
+      items: 1
+    },
+    767: {
+      items: 2
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
+
+// <!-- <<<<<<<<<<<<< our testimonials >>>>>>>>>>>>>> -->
+$(".company-logo").owlCarousel({
+  autoplay: true,
+  slideSpeed: 1000,
+  items: 3,
+  loop: true,
+  nav: true,
+  navText: [
+    '<i class="fa fa-arrow-left fa-2x"></i>',
+    '<i class="fa fa-arrow-right fa-2x"></i>'
+  ],
+  margin: 30,
+  dots: true,
+  responsive: {
+    320: {
+      items: 1
+    },
+    767: {
+      items: 2
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
